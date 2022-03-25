f9ac6f4b50ebec9a53ffdb71d46841952c7f7b8f
Justin Hu <justin.hu.rj@gmail.com>
2022-03-25 14:54:31 +0800

    Add React-Bootstrap (not the same as bootstrap) and set toastr and redirect for login error and success handling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,22 @@
 import {useState} from 'react';
 import './Login.css';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { Toast, ToastContainer } from "react-bootstrap";
 
 const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errorOccurred, setErrorOccurred] = useState(false);
+    const history = useHistory();
 
 
     const loginUser = (event) => {
         event.preventDefault();
+        if(true) {
+            setErrorOccurred(true);
+            // history.replace('/registration');
+        }
     }
 
     const emailChangeHandler = (event) => {
@@ -20,21 +27,34 @@ const Login = () => {
         setPassword(event.target.value);
     }
 
+    const removeErrorToastr = () => {
+        setErrorOccurred(false);
+    }
+
+
     return (
         <>
+            <div aria-live="polite" aria-atomic="true" className="position-relative">
+                <ToastContainer className="p-3" position='top-center'>
+                    <Toast show={errorOccurred} onClose={removeErrorToastr} bg='danger' delay={5000} autohide>
+                        <Toast.Header closeButton={true}>
+                            <strong className="me-auto">Error</strong>
+                        </Toast.Header>
+                        <Toast.Body className='text-white'>Email may not be registered or Password is incorrect.</Toast.Body>
+                    </Toast>
+                </ToastContainer>
+            </div>
             <div className='jumbotron vertical-center'>
                 <div className='container'>
                     <h1>Login</h1>
                     <form onSubmit={loginUser}>
-                        <div className="mb-3">
+                        <div className="form-floating mb-3">
+                            <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="name@example.com" onChange={emailChangeHandler}/>
                             <label htmlFor="inputEmail" className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp"
-                                   placeholder="name@example.com" onChange={emailChangeHandler}/>
                         </div>
-                        <div className="mb-3">
+                        <div className="form-floating mb-3">
+                            <input type="password" className="form-control" id="inputPassword" onChange={passwordChangeHandler} placeholder='Password'/>
                             <label htmlFor="inputPassword" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1"
-                                   onChange={passwordChangeHandler}/>
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
