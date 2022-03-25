8d6dabde4a07f92f5ab39214c24deb5982c45fb2
Justin Hu <justin.hu.rj@gmail.com>
2022-03-26 00:50:03 +0800

    merge with registration form

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Redirect} from 'react-router-dom';
+import Login from './components/Login/Login';
 import React from 'react';
 import { Container } from "semantic-ui-react";
 import Header from "./components/Registration/header";
 import RegistrationForm from "./components/Registration/registrationForm";
 
 function App() {
-  return (
-    <div className="App">
-        <Header />
-		<RegistrationForm/>
-    </div>
-  );
+    return (
+        <>
+        	<Header />
+            <Route path="/" exact>
+                <Redirect to='/login'/>
+            </Route>
+            <Route path='/login'>
+                <Login/>
+            </Route>
+            <Route path='/registration'>
+                <RegistrationForm/>
+            </Route>
+        </>
+    );
 }
 
 export default App;
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,77 @@
+import { useState, useContext } from 'react';
+import './Login.css';
+import { Link, useHistory } from "react-router-dom";
+import { Toast, ToastContainer } from "react-bootstrap";
+import AuthContext from "../util/auth-context";
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorOccurred, setErrorOccurred] = useState(false);
+    const history = useHistory();
+
+    const authCtx = useContext(AuthContext);
+
+    const loginUser = (event) => {
+        event.preventDefault();
+        let hasError = false;
+        if(email === "" || password === "") {
+            hasError = true;
+        }
+        if(hasError) {
+            setErrorOccurred(true);
+            return;
+        }
+        authCtx.login('faketoken');
+        history.replace('/registration');
+    }
+
+    const emailChangeHandler = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const passwordChangeHandler = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const removeErrorToastr = () => {
+        setErrorOccurred(false);
+    }
+
+
+    return (
+        <>
+            <div aria-live="polite" aria-atomic="true" className="position-relative">
+                <ToastContainer className="p-3" position='top-center'>
+                    <Toast show={errorOccurred} onClose={removeErrorToastr} bg='danger' delay={5000} autohide>
+                        <Toast.Header closeButton={true}>
+                            <strong className="me-auto">Authentication Failed</strong>
+                        </Toast.Header>
+                        <Toast.Body className='text-white'>Email may not be registered or Password is incorrect.</Toast.Body>
+                    </Toast>
+                </ToastContainer>
+            </div>
+            <div className='jumbotron vertical-center'>
+                <div className='container'>
+                    <h1>Login</h1>
+                    <form onSubmit={loginUser}>
+                        <div className="form-floating mb-3">
+                            <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="name@example.com" onChange={emailChangeHandler}/>
+                            <label htmlFor="inputEmail" className="form-label">Email address</label>
+                        </div>
+                        <div className="form-floating mb-3">
+                            <input type="password" className="form-control" id="inputPassword" onChange={passwordChangeHandler} placeholder='Password'/>
+                            <label htmlFor="inputPassword" className="form-label">Password</label>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                    <br/>
+                    <Link to='/registration' className="link-primary">Sign up for an Account now!</Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login;
