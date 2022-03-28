99602541e90cc057fa9a4e3066683bdbcd7d3016
Justin Hu <justin.hu.rj@gmail.com>
2022-03-28 19:34:22 +0800

    add JWT tokenization for login cookie

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,7 @@ module server
 go 1.18
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
 	go.mongodb.org/mongo-driver v1.8.4
--- server/login/login.go
+++ server/login/login.go
@@ -2,10 +2,19 @@ package login
 
 import (
 	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
 	"log"
 	"net/http"
 	"server/dbconnection"
+	"time"
 )
+
+var jwtKey = []byte("secret_key")
+
+type Claims struct {
+	Username string
+	jwt.StandardClaims
+}
 
 type userResult struct {
 	Success   int
@@ -29,25 +38,44 @@ func User(w http.ResponseWriter, r *http.Request) {
 	var result userResult
 	// flag as error if there is a problem with the extraction
 	if err != nil {
-		log.Println(err)
 		result.Success = 0
 		result.ErrorMsg = "Backend Validation Fail. Please contact Administrator"
 	} else {
 		record, error := dbconnection.RetrieveHomeOwner(t.Email)
 		if error != nil {
-			log.Println(error)
 			result.Success = 0
-			result.ErrorMsg = "No such Email was found. Please Register"
+			result.ErrorMsg = "Incorrect Email or Password Credentials"
+			w.WriteHeader(http.StatusUnauthorized)
 		} else {
 			if record.Email == t.Email && record.Password == t.Password {
 				result.Success = 1
 				result.FirstName = record.FirstName
 				result.LastName = record.LastName
-				//TODO: assign Login Key for authentication purposes
-				result.LoginKey = "123"
+				expirationTime := time.Now().Add(time.Minute * 20)
+				claims := &Claims{
+					Username: t.Email,
+					StandardClaims: jwt.StandardClaims{
+						ExpiresAt: expirationTime.Unix(),
+					},
+				}
+				token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+				tokenString, err := token.SignedString(jwtKey)
+				if err != nil {
+					result.Success = 0
+					result.ErrorMsg = "Trouble with JWT token signing"
+					w.WriteHeader(http.StatusGatewayTimeout)
+				} else {
+					result.LoginKey = tokenString
+					http.SetCookie(w, &http.Cookie{
+						Name:    "token",
+						Value:   tokenString,
+						Expires: expirationTime,
+					})
+				}
 			} else {
 				result.Success = 0
 				result.ErrorMsg = "Incorrect Email or Password Credentials"
+				w.WriteHeader(http.StatusUnauthorized)
 			}
 		}
 	}
